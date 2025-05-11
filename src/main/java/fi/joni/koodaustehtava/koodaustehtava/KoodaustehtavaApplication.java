207d01d879bd054a6144ead68729aa9d1039ab50
Joni Karlsson <karlsson.joni@gmail.com>
2025-05-11 12:53:18 +0300

    Initial commit

---
+++ src/main/java/fi/joni/koodaustehtava/koodaustehtava/KoodaustehtavaApplication.java
@@ -0,0 +1,13 @@
+package fi.joni.koodaustehtava.koodaustehtava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoodaustehtavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoodaustehtavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/joni/koodaustehtava/koodaustehtava/KoodaustehtavaApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.joni.koodaustehtava.koodaustehtava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoodaustehtavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
